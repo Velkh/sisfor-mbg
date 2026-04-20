d42c32ce0a21190e8536cc61ee44b8f4a7423928
Velkh <bagussaju17@gmail.com>
2026-04-20 22:23:29 +0700

    Enhance FotoSppg and MenuSppg models with fillable properties and relationships

--- app/Models/FotoSppg.php
+++ app/Models/FotoSppg.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FotoSppg extends Model
 {
-    /** @use HasFactory<\Database\Factories\FotoSppgFactory> */
     use HasFactory;
+
+    protected $table = 'fotosppg';
+    protected $primaryKey = 'id_foto';
+
+    protected $fillable = [
+        'id_sppg',
+        'foto_sppg',
+    ];
+
+    public function sppg(): BelongsTo
+    {
+        return $this->belongsTo(Sppg::class, 'id_sppg', 'id_sppg');
+    }
 }
--- app/Models/MenuSppg.php
+++ app/Models/MenuSppg.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MenuSppg extends Model
 {
-    /** @use HasFactory<\Database\Factories\MenuSppgFactory> */
     use HasFactory;
+
+    protected $table = 'menusppg';
+    protected $primaryKey = 'id_menu';
+
+    protected $fillable = [
+        'id_sppg',
+        'nama_menu',
+        'foto_menu',
+    ];
+
+    public function sppg(): BelongsTo
+    {
+        return $this->belongsTo(Sppg::class, 'id_sppg', 'id_sppg');
+    }
 }
